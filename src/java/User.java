53b6575ca02465c8c1c4f2991842036e6ad3f84c
nnadlin <53036225+nnadlin@users.noreply.github.com>
2020-03-24 20:14:36 +0800

    CRUD Servlet

---
+++ src/java/User.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Makmal PPIMG
+ */
+public class User {
+    private int id;
+    private String username, password, role;
+    
+    public int getId(){
+        return id;
+    }
+    
+    public void setId(int id){
+        this.id=id;
+    }
+    
+    public String getUsername(){
+        return username;
+    }
+    
+    public void setUsername(String username){
+        this.username=username;
+    }
+    
+        public String getPassword(){
+        return password;
+    }
+    
+    public void setPassword(String password){
+        this.password=password;
+    }
+    
+    public String getRole(){
+        return role;
+    }
+    
+    public void setRole(String role){
+        this.role=role;
+    }
+    
+    
+}
